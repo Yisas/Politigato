f21f756b82a64d9a4346dd7a3948feebc1ed6521
Jesus Imery <yisas_0929@hotmail.com>
2019-02-11 08:33:40 -0500

    Moving cat

---
+++ Assets/Scrpits/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(horizontalSpeed, 0));
+    }
+}
