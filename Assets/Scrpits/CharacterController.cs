81577ee6376960c36ad2ea436875f2eccbed6a58
Jesus Imery <yisas_0929@hotmail.com>
2019-02-11 09:07:49 -0500

    Player respawn

--- Assets/Scrpits/CharacterController.cs
+++ Assets/Scrpits/CharacterController.cs
@@ -8,6 +8,7 @@ public class CharacterController : MonoBehaviour
     public float horizontalSpeed;
     public float verticalSpeed;
 
+    private Vector3 initialPosition;
     private float verticalInput;
 
     private Rigidbody2D rb;
@@ -16,6 +17,7 @@ public class CharacterController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -27,5 +29,13 @@ public class CharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalSpeed, verticalInput * verticalSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Obstacle")
+        {
+            transform.position = initialPosition;
+        }
     }
 }
