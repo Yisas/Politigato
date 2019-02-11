bfed1a9de58b56949637abeb62e6d1eff08d81c7
Jesus Imery <yisas_0929@hotmail.com>
2019-02-11 10:49:38 -0500

    Main menu

---
+++ Assets/Scrpits/CustomButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CustomButton : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip focusSound;
+    public AudioClip clickSound;
+
+    private Animator animator;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = FindObjectOfType<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(focusSound);
+    }
+
+    public void StartGame()
+    {
+        animator.SetTrigger("fade");
+        audioSource.PlayOneShot(clickSound);
+    }
+}
