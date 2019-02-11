47da3291f2f1bb327e04423658be317259aeff66
Jesus Imery <yisas_0929@hotmail.com>
2019-02-11 09:01:36 -0500

    Cat flying through pipes - vectical input

--- Assets/Scrpits/CharacterController.cs
+++ Assets/Scrpits/CharacterController.cs
@@ -8,6 +8,8 @@ public class CharacterController : MonoBehaviour
     public float horizontalSpeed;
     public float verticalSpeed;
 
+    private float verticalInput;
+
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -19,11 +21,11 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-               
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector2(horizontalSpeed, 0));
+        rb.velocity = new Vector2(horizontalSpeed, verticalInput * verticalSpeed);
     }
 }
